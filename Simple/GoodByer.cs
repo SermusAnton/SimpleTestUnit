2de02e3e792de3617d958b07a40f09a75d611037
SermusAnton <antonsermus@mail.ru>
2018-07-12 22:03:46 +0300

    Fist init

---
+++ Simple/GoodByer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple {
+    public class GoodByer {
+        private string who;
+        public string Message {
+            get { return "Привет "+who; }
+            set {
+                if (value == "") {
+                    throw new Exception("No person to say goodbay!!!");
+                }
+                who = value;
+            }
+        }
+        public GoodByer() {
+            who = "Anonim";
+        }
+        public GoodByer(string who) {
+            if (who == "") {
+                throw new Exception("No person to say goodbay!!!");
+            }
+            this.who = who;
+        }
+    }
+}
---
+++ TestSimple/TestGoodByer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple;
+using NUnit.Framework;
+
+namespace TestSimple
+{
+    [TestFixture]
+    public class TestGoodByer
+    {
+        /// <summary>
+        /// Тест пустого конструктора
+        /// </summary>
+        [Test()]
+        public void TestConstructorWithParams() {
+            Assert.Throws<Exception>( delegate(){ GoodByer goodByer = new GoodByer(""); });
+        }
+        /// <summary>
+        /// Тест конструктора с параметром
+        /// </summary>
+        [Test()]
+        public void TestConstructor() {
+            GoodByer goodByer = new GoodByer("Jhon");
+            Assert.AreEqual("Привет Jhon",goodByer.Message);
+        }
+        /// <summary>
+        /// Тест свойства Message String.Empty 
+        /// </summary>
+        [Test()]
+        public void TestSetWhoEmpty() {
+            GoodByer goodByer = new GoodByer();
+            Assert.Throws<Exception>(delegate () { goodByer.Message=""; });
+        }
+        /// <summary>
+        /// Тест свойства Message задаем значение 
+        /// </summary>
+        [Test()]
+        public void TestSetWhoNotEmpty() {
+            GoodByer goodByer = new GoodByer();
+            goodByer.Message = "Jhon";
+            Assert.AreEqual("Привет Jhon",goodByer.Message);
+        }
+    }
+}
